d5385b686aee3050898d9f57256bbcb1b7a3dac2
Basti <basti.babaeva@yandex.ru>
2025-03-25 21:48:50 +0700

    работа над клиентским кодом
    создание объекта одиночки, создание сигнально-слотных связей

--- lab1_qt/main.cpp
+++ lab1_qt/main.cpp
@@ -1,8 +1,21 @@
 #include <QCoreApplication>
-
+#include <QString>
+#include "fileInfo.h"
+#include "fileManager.h"
+#include "fileLogger.h"
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    fileInfo& instance=fileInfo::Instance();
+    fileManager f;
+    QObject::connect(&instanse, &fileManager::fileExist, &f, &fileLogger::printExist);
+    QObject::connect(&instanse, &fileManager::fileChanged, &f, &fileLogger::printChange);
+    QObject::connect(&instanse, &fileManager::fileDeleted, &f, &fileLogger::printDelete);
+
+    QString f1;
+    cout<<"Enter name of file: ";
+    f1<<cin;
+    f.addFile(f1);
     return a.exec();
 }
